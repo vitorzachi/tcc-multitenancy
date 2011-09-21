796b1d8224299b0aa9d8dd73062c94a166dcf43d
vitorzachi@gmail.com <vitorzachi@gmail.com@27fd352f-d3e2-901b-b1f2-ccc50a194ca8>
2011-09-21 23:44:07 +0000

    alterações para possibilitar futuramente diferentes implementações de queryProcessor

--- tccMultitenancy/src/br/edu/unoesc/tcc/queryProcessor/QueryParameter.java
+++ tccMultitenancy/src/br/edu/unoesc/tcc/queryProcessor/QueryParameter.java
@@ -1,66 +1,12 @@
 package br.edu.unoesc.tcc.queryProcessor;
 
 /**
+ * Interface que representa um parâmetro a ser adicionado na cláusula "where" de
+ * um SQL.
  * 
  * @author vitor
  */
-public class QueryParameter {
+public interface QueryParameter {
 
-	private String paramName;
-	private String paramValue;
-
-	public QueryParameter(String paramName, String paramValue) {
-		this.paramName = paramName;
-		this.paramValue = paramValue;
-	}
-
-	public QueryParameter() {
-	}
-
-	public String getParamName() {
-		return paramName;
-	}
-
-	public String getParamValue() {
-		return paramValue;
-	}
-
-	public void setParamName(String paramName) {
-		this.paramName = paramName;
-	}
-
-	public void setParamValue(String paramValue) {
-		this.paramValue = paramValue;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null) {
-			return false;
-		}
-		if (getClass() != obj.getClass()) {
-			return false;
-		}
-		final QueryParameter other = (QueryParameter) obj;
-		if ((this.paramName == null) ? (other.paramName != null)
-				: !this.paramName.equals(other.paramName)) {
-			return false;
-		}
-		if (this.paramValue != other.paramValue
-				&& (this.paramValue == null || !this.paramValue
-						.equals(other.paramValue))) {
-			return false;
-		}
-		return true;
-	}
-
-	@Override
-	public int hashCode() {
-		int hash = 7;
-		hash = 23 * hash
-				+ (this.paramName != null ? this.paramName.hashCode() : 0);
-		hash = 23 * hash
-				+ (this.paramValue != null ? this.paramValue.hashCode() : 0);
-		return hash;
-	}
+	String getWhereRestriction();
 }
--- tccMultitenancy/src/javaapplication30/Main.java
+++ tccMultitenancy/src/javaapplication30/Main.java
@@ -15,27 +15,39 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		// inicializando TenantContext
 		TenantContext.setUp("JavaApplication30PU", "javaapplication30");
 
+		// criando empresa
 		Empresa e = new Empresa();
-		e.setNome("TESTE");
+		e.setNome("TESTE " + System.currentTimeMillis());
 
+		// salvando a empresa no banco de dados
 		TenantContext.getEntityManager().getTransaction().begin();
 		TenantContext.getEntityManager().persist(e);
+		System.out.println("salvei empresa nome:" + e.getNome());
 		TenantContext.getEntityManager().getTransaction().commit();
 
+		// setando a empresa criada anteriormente como sendo a proprietária dos
+		// registros a partir de agora
 		TenantContext.setTenantOwner(e);
 
+		// selecionando todas as empresas existentes.
+		// não pode inserir cláusula where pois ela não estende
+		// AbstractTenantModel
 		System.out.println(TenantContext.getEntityManager().createQuery("select e from Empresa e")
 				.getResultList());
 
+		// criando um cliente
 		Cliente c = new Cliente();
-		c.setNome("clt");
-
+		c.setNome("cliente");
+		// salvando o cliente no banco de dados
 		TenantContext.getEntityManager().getTransaction().begin();
 		TenantContext.getEntityManager().persist(c);
 		TenantContext.getEntityManager().getTransaction().commit();
 
+		// listando clientes(da empresa que foi setada anteriormente como sendo
+		// a proprietária dos registros)
 		System.out.println(TenantContext.getEntityManager().createQuery("select e from Cliente e")
 				.getResultList());
 	}
