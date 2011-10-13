5b8868831142b2e860cc53e7dd0ff68f4f389dc4
vitorzachi@gmail.com <vitorzachi@gmail.com@27fd352f-d3e2-901b-b1f2-ccc50a194ca8>
2011-10-13 20:17:07 +0000

    inserido classes do trugger

---
+++ tccMultitenancy/src/br/edu/unoesc/alligator/webmodule/filter/WebModuleUtils.java
@@ -0,0 +1,8 @@
+package br.edu.unoesc.alligator.webmodule.filter;
+
+public class WebModuleUtils {
+
+	public static final String ARG_PACKAGE_NAME = "packageName";
+	public static final String ARG_PERSISTENCE_UNIT_NAME = "persistenceUnitName";
+
+}
